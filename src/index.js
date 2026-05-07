e3f06bd2a0678090c4b73e54133deca44255a9da
Adamu Ismaila <ajimeta@outlook.com>
2026-05-07 21:57:02 +0100

    Restore PTAD UAT application

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>PTAD UAT Portal</h1>
-      <p>The application is now rendering successfully.</p>
-    </div>
-  );
-}
+import App from "./PTAD_UAT_App_Enhanced";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
