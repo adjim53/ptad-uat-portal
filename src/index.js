cd636516b42a1e300ae7c4d62dfe99a8af6527da
Adamu Ismaila <ajimeta@outlook.com>
2026-05-07 22:29:14 +0100

    Add Tailwind CSS file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./PTAD_UAT_App_Enhanced";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
